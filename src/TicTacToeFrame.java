95d029044dccf786612d9aa12f1ec80c61bc993b
cwagr <agricocw@mail.uc.edu>
2024-02-22 12:36:40 -0500

    Final

--- src/TicTacToeFrame.java
+++ src/TicTacToeFrame.java
@@ -5,14 +5,16 @@ import java.awt.event.ActionListener;
 
 public class TicTacToeFrame extends JFrame {
     private JButton[][] buttons;
-    private TicTacToeRunner game;
+    private String[][] board;
+    private String currentPlayer;
 
     public TicTacToeFrame() {
         setTitle("Tic Tac Toe");
         setSize(300, 300);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        game = new TicTacToeRunner();
+        currentPlayer = "X";
+        board = new String[3][3];
 
         JPanel panel = new JPanel(new GridLayout(3, 3));
         buttons = new JButton[3][3];
@@ -24,20 +26,11 @@ public class TicTacToeFrame extends JFrame {
                 buttons[row][col].setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 40));
                 buttons[row][col].addActionListener(listener);
                 panel.add(buttons[row][col]);
+                board[row][col] = " ";
             }
         }
 
-        JButton quitButton = new JButton("Quit");
-        quitButton.addActionListener(e -> {
-            int option = JOptionPane.showConfirmDialog(null, "Are you sure you want to quit?", "Quit",
-                    JOptionPane.YES_NO_OPTION);
-            if (option == JOptionPane.YES_OPTION) {
-                System.exit(0);
-            }
-        });
-
         add(panel, BorderLayout.CENTER);
-        add(quitButton, BorderLayout.SOUTH);
         setVisible(true);
     }
 
@@ -47,18 +40,17 @@ public class TicTacToeFrame extends JFrame {
             for (int row = 0; row < 3; row++) {
                 for (int col = 0; col < 3; col++) {
                     if (e.getSource() == buttons[row][col]) {
-                        if (game.makeMove(row, col)) {
-                            buttons[row][col].setText(game.getCurrentPlayer());
-                            if (game.checkWin()) {
-                                JOptionPane.showMessageDialog(null, game.getCurrentPlayer() + " wins!");
-                                game.reset();
-                                resetButtons();
-                            } else if (game.checkTie()) {
+                        if (board[row][col].equals(" ")) {
+                            buttons[row][col].setText(currentPlayer);
+                            board[row][col] = currentPlayer;
+                            if (checkWin()) {
+                                JOptionPane.showMessageDialog(null, currentPlayer + " wins!");
+                                handleEndOfGame();
+                            } else if (checkTie()) {
                                 JOptionPane.showMessageDialog(null, "It's a tie!");
-                                game.reset();
-                                resetButtons();
+                                handleEndOfGame();
                             } else {
-                                game.switchPlayer();
+                                switchPlayer();
                             }
                         } else {
                             JOptionPane.showMessageDialog(null, "Invalid move. Try again.");
@@ -69,11 +61,61 @@ public class TicTacToeFrame extends JFrame {
         }
     }
 
-    private void resetButtons() {
+    private void reset() {
         for (int row = 0; row < 3; row++) {
             for (int col = 0; col < 3; col++) {
                 buttons[row][col].setText("");
+                board[row][col] = " ";
             }
+        }
+        currentPlayer = "X";
+    }
+
+    private void switchPlayer() {
+        currentPlayer = (currentPlayer.equals("X")) ? "O" : "X";
+    }
+
+    private boolean checkWin() {
+
+        for (int i = 0; i < 3; i++) {
+            if (board[i][0].equals(currentPlayer) && board[i][1].equals(currentPlayer) && board[i][2].equals(currentPlayer)) {
+                return true;
+            }
+        }
+
+        for (int i = 0; i < 3; i++) {
+            if (board[0][i].equals(currentPlayer) && board[1][i].equals(currentPlayer) && board[2][i].equals(currentPlayer)) {
+                return true;
+            }
+        }
+
+        if (board[0][0].equals(currentPlayer) && board[1][1].equals(currentPlayer) && board[2][2].equals(currentPlayer)) {
+            return true;
+        }
+        if (board[0][2].equals(currentPlayer) && board[1][1].equals(currentPlayer) && board[2][0].equals(currentPlayer)) {
+            return true;
+        }
+        return false;
+    }
+
+    private boolean checkTie() {
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                if (board[row][col].equals(" ")) {
+                    return false;
+                }
+            }
+        }
+        return !checkWin();
+    }
+
+    private void handleEndOfGame() {
+        int option = JOptionPane.showConfirmDialog(null, "Do you want to play another game?", "Play Again",
+                JOptionPane.YES_NO_OPTION);
+        if (option == JOptionPane.YES_OPTION) {
+            reset();
+        } else {
+            System.exit(0);
         }
     }
 
@@ -81,4 +123,3 @@ public class TicTacToeFrame extends JFrame {
         SwingUtilities.invokeLater(TicTacToeFrame::new);
     }
 }
-
